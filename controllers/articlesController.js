c9cf4c97f6a2bc2eb5076cdcc5baa0018ab52949
Felipe Jose Llerena Catalan <llerenafelipe7@gmail.com>
2020-11-12 23:38:56 -0500

    omnix images settings

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -1,4 +1,6 @@
 const Article = require('./../models/Article')
+const imageUpload = require('./../lib/imageUpload')
+const { v4: uuidv4 } = require('uuid');
 
 exports.allArticles = async (req, res, next) => {
   let articles = await Article.find()
@@ -16,15 +18,20 @@ exports.getArticle = async (req, res, next) => {
 }
 
 exports.createArticle = async (req, res, next) => {
-  const { article } = req.body
+  let { article } = req.body
+  article.image = await imageUpload(article.image, {name: `${article.title}-${uuidv4()}`})
   let art = new Article(article);
   let result = await art.save();
   res.json({article: result})
 }
 
 exports.updateArticle = async (req, res, next) => {
-  const { id } = req.params
-  const { article } = req.body
+  let { id } = req.params
+  let { article } = req.body
+  if (!article.image.includes("http")) {
+    let urlImage = await imageUpload(article.image, {name: `${article.title}-${uuidv4()}`})
+    article.image = urlImage
+  }
   let art = await Article.findById(id)
   await art.updateOne(article)
   res.json({article: art})
