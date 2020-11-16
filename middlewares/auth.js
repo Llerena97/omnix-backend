a23128b7999e9298b8a030678fedf161a4b01c84
Felipe Jose Llerena Catalan <llerenafelipe7@gmail.com>
2020-11-16 17:53:41 -0500

    reinforcing auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -8,9 +8,11 @@ module.exports = (req, res, next) => {
       const token = authHeader.split(' ')[1]
       const user = jwt.verify(token, process.env.SECRET_SIGN)
       req.user = user
+      return next()
     } catch (error) {
       console.log(':: ERROR ::', error)
+      return res.status(401).send({ msg: 'token invalido.' });
     }
   }
-  return next()
+  res.status(401).json({ msg: 'Acceso Denegado.' });
 }
